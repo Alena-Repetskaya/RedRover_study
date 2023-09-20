f299e41253c1d1c2b272c506bc6c9ff52de2c4ad
Alena-Repetskaya <repeclaya.alyona@gmail.com>
2023-09-20 08:56:23 +0400

    Study, homework_4

--- Homework_4.py
+++ Homework_4.py
@@ -3,10 +3,15 @@
 # def square(a):
 #     b = 4 * a
 #     c = a * a
-#     d = (2 ** 0.5) * a
+#     d = round((2 ** 0.5) * a)
 #     return (b, c, d)
 # a = int(input('Введи целое число: '))
 # print(square(a))
+#
+# # variant from teacher
+# a = int(input('Введи целое число: '))
+# print((4 * a, a * a, round(a * (2 ** 0.5))))
+
 
 '''4.2. Напишите фукнцию, которая принимает произвольное количество именнованных аргументов и выводит их построчно
      в формате аргумент: значение. Например:
@@ -14,12 +19,16 @@
 	last_name: Smith
 	age: 35
 	position: web developer'''
+
+# my_dict = {
+# 'name': 'John',
+# 'last_name': 'Smith',
+# 'age': 35,
+# 'position': 'web developer'}
 # def task_2(**kwargs):
-#     task_2 = {'name': 'John', 'last_name': 'Smith', 'age': 35, 'position': 'web developer'}
-#     for key, value in kwargs.items():
-#         print(f'{key}: {value} \n')
-# task_2 = {'name':'John','last_name':'Smith','age': 35,'position':'web developer'}
-# print(task_2)
+#     print(*kwargs.items(), sep='\n')
+#
+# task_2(name='John', last_name='Smith', age=35, position='web developer')
 
 '''4.3. Используя лямбда-выражение, из списка my_list = [20, -3, 15, 2, -1, -21] создайте новый список, содержащий только
      положительные числа'''
@@ -37,9 +46,9 @@
 # import time
 # def timer(f):
 #     def tmp(*args, **kwargs):
-#         t = time.time()
+#         t = time.perf_counter()
 #         res = f(*args, **kwargs)
-#         print (time.time()-t)
+#         print(time.perf_counter())
 #         return res
 #
 #     return tmp
@@ -52,11 +61,15 @@
 '''4.6. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические вычисления.
      Примените эти функции в качестве методов в другом файле. '''
 
-# from my_calc import sum
-# from my_calc import sub
-# from my_calc import mult
-# from my_calc import div
-#
+from my_calc import sum
+from my_calc import sub
+from my_calc import mult
+from my_calc import div
+
+# var2 -  можно вызвать все фунукции сразу:
+from my_calc import *
+
+
 # a = int(input('Введи число: '))
 # b = int(input('Введи число: '))
 #
